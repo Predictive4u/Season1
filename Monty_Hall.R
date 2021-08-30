05f37066e6c5637585fb71bd66151a4e6bc739ed
Hanwen Zhang <hanwen@Andress-MacBook-Pro-3.local>
2021-08-30 14:36:22 -0400

    Paradoja de Monty Hall listo

--- Monty_Hall.R
+++ Monty_Hall.R
@@ -1,14 +1,20 @@
 set.seed(20210808)
-n.sim <- 1000000 # número de simulaciones
-k <- 3 # número de puertas
+# número de simulaciones
+n.sim <- 1000000 
+# número de puertas
+k <- 3 
 escenarios <- seleccion <- matrix(0, n.sim, k)
 for(i in 1:n.sim){
-  j <- sample(1:k, 1) # el número de puerta con premio
+  # el número de puerta con premio
+  j <- sample(1:k, 1) 
   escenarios[i,j] <- 1
-  m <- sample(1:k, 1) # el número de puerta elegida
+  # el número de puerta seleccionada
+  m <- sample(1:k, 1) 
   seleccion[i,m] <- 1
 }
-# probabilidad de ganar al quedar con la selección inicial
+# probabilidad de ganar 
+# al quedar con la selección inicial
 prob1 <- mean(rowSums(escenarios * seleccion)) 
-# probabilidad de ganar al cambiar la selección
+# probabilidad de ganar 
+# al cambiar la selección
 prob2 <- 1 - prob1

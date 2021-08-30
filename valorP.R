dfa432800f72bc2c1b05d70bd131d202992b57c8
Hanwen Zhang <hanwen@Andress-MacBook-Pro-3.local>
2021-08-30 14:36:04 -0400

    Códigos del video de valor p

---
+++ valorP.R
@@ -0,0 +1,21 @@
+# 3. El valor p no indica qué tan compatible son los datos con la hipótesis nula, 
+# sino qué tan compatible con el modelo estadístico
+set.seed(20210827)
+n.sim <- 10000
+count <- 0
+n <- 20
+lambda <- 0.1
+for(i in 1:n.sim){
+  x <- rpois(n, lambda)
+  count <- count + ifelse(t.test(x, mu = lambda)$p.value < 0.05, 1, 0)
+}
+count/n.sim
+
+# 4. Traslape de dos IC no implica que las medias sean iguales
+set.seed(20210827)
+x1 <- rnorm(50, 5, 1)
+x2 <- rnorm(50, 4.2, 1)
+t.test(x1, x2, var.equal = T)
+t.test(x1)
+t.test(x2)
+

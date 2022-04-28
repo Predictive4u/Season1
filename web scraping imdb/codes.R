21986337734ff3d2f862d460be4217132af80a9a
Predictive4u <predictive4u@gmail.com>
2022-04-28 16:04:24 -0400

    Códigos web scraping IMDB completo.

--- web scraping imdb/codes.R
+++ web scraping imdb/codes.R
@@ -1,17 +1,18 @@
+rm(list=ls())
 library(rvest)
 library(tidyverse)
-
 
 # Bajar información de las 10 mil películas mejor calificados de género DRAMA de IMDb
 
 n.resultados <- 10000
-
 n.por.pagina <- 50
+
+url <- "https://www.imdb.com/search/title/?title_type=movie&genres=drama&sort=user_rating,desc&explore=title_type,genres"
 
 url1 <- "https://www.imdb.com/search/title/?title_type=movie&genres=drama&sort=user_rating,desc&start="
 url2 <- "&explore=title_type,genres&ref_=adv_nxt"
-secuencia <- seq(from=1, to = (n.resultados-n.por.pagina+1), by=50)
-url.all <- str_c(url1, secuencia, url2)
+
+url.all <- str_c(url1, seq(from=1, to = (n.resultados-n.por.pagina+1), by=50), url2)
 
 prueba <- url.all[1]
 
@@ -68,6 +69,58 @@ extraer_year <- function(html){
     as.numeric()  
 }
 
+# Extraer el género  
+extraer_genero <- function(html){
+  genero <- html %>%
+    read_html() %>%
+    html_nodes('.genre') %>%
+    html_text()
+  genero2 <- genero %>% gsub("\\\n", "", .) %>% # eliminar \n 
+    gsub(" ","" , .) # eliminar los espacios
+  
+  p <- length(genero2)
+  res <- matrix(NA, p, 3) # 3 es el número máximo de géneros para cada película
+  for(i in 1:p){
+    aux <- strsplit(genero2, split = ",")[[i]]
+    res[i, 1:length(aux)] <- aux
+  }
+  res
+}
+
+# Extraer el ranking y el director (es que en el director hay missing)
+extraer_director <- function(html){
+  info <- html %>%
+    read_html() %>%
+    html_nodes('.text-primary , .text-muted+ p a:nth-child(1)') %>%
+    html_text()
+
+  # si no hay missing en el director, entonces se alterna número y nombre
+  # si hay números consecutivos, entonces sí hay missing en el directors
+  ind <- c()
+  for(j in 1:length(info)){
+    letras <- c(letters, LETTERS)
+    ind[j] <- prod(str_detect(info[j], letras, negate = TRUE))
+    # valor 1 indica que solo contiene números, valor 0 indica que contiene letras, es nombre del director
+  }
+  m <- which(diff(ind)==0)
+  
+  if(length(m) == 0){ # si no hay missing en director
+    director <- html %>%
+      read_html() %>%
+      html_nodes('.text-muted+ p a:nth-child(1)') %>%
+      html_text()
+  }
+  
+  if(length(m) > 0){ # si hay missing en director
+    for(jj in 1:length(m)){
+      info <- c(info[1:(m[jj] + jj - 1)],NA, info[-(1:(m[jj]+ jj -1))])  
+    }
+    director <- info[seq(2, length(info), 2)]
+  }
+
+  director
+}
+
 
 # Combinar información de todas las variables
 extraer_info <- function(html){
@@ -76,16 +129,22 @@ extraer_info <- function(html){
   rate <- extraer_rate(html)
   year <- extraer_year(html)
   title <- extraer_title(html)
+  genero <- extraer_genero(html)
+  director <- extraer_director(html)
   data.frame(ranking = ranking,
              nombre = title,
              calificacion = rate,
              votos = vote, 
-             ano = year)
+             ano = year,
+             director = director,
+             genero = genero)
 }
 
 # Extraer información de todos las páginas
-extraer_url <- function(url){
-  url %>% map(extraer_info) %>% bind_rows()
+extraer_url <- function(url.all){
+  url.all %>% map(extraer_info) %>% bind_rows()
 }
 
+
 x <- extraer_url(url.all)
+

fd4aaf9108c758dca11bc4aeeac7e386b4e2881f
Predictive4u <predictive4u@gmail.com>
2022-05-29 20:59:26 -0400

    Uso del paquete DataEditR

--- DataEditR/EditarDatos.R
+++ DataEditR/EditarDatos.R
@@ -1,13 +1,32 @@
 rm(list=ls())
-direccion <- "https://github.com/Predictive4u/Season1/blob/main/Mazda_20211112.rda?raw=TRUE"
+direccion <- "https://github.com/Predictive4u/Season1/blob/main/DataEditR/Jeep_RM_20220518.rda?raw=TRUE"
 load(url(direccion))
 
-# Opción 1. 
-mazda2 <- edit(mazda)
+# Instalar el paquete
+install.packages("DataEditR")
+library(DataEditR)
 
-# Opción 2.
+# con tibble no funciona muy bien
+jeep <- as.data.frame(jeep)
+# Definir la nueva base de datos
+jeep.nuevo <- data_edit(x = jeep)
 
 
-library(editData)
-mazda
-result <- editData(mtcars)
+# OJO!!!!
+# 1. No usar tibble, convertir a data.frame
+# precio menor a 6 millones
+
+# 2. No usar más de 2 filtros
+jeep %>% filter(precio < 6000000,
+                modelo2 == "Jeep Grand Cherokee", 
+                año < 2005,
+                año > 2000) 
+
+jeep.nuevo2 <- data_edit(x = jeep.nuevo)
+
+# 3. Cuando no hay ningún registro que cumple con las condiciones, no funciona!!!
+jeep %>% filter(precio < 15000000,
+                      modelo2 == "Jeep Compass", 
+                      año > 2017) %>%
+  dim()
+
---
+++ jeep.r
@@ -0,0 +1,13 @@
+structure(list(precio = c(5e+06, 5200000, 4300000, 5780000, 4500000, 
+4500000, 3800000), km = c(135000, 197, 222200, 220000, 120000, 
+160000, 1e+05), año = c(2010L, 2002L, 1998L, 2002L, 2004L, 2004L, 
+1967L), modelo = c(" Jeep Grand Cherokee 4.7 Limited Auto", " Jeep Cherokee ", 
+" Jeep Grand Cherokee ", " Jeep Grand Cherokee Laredo", " Jeep Grand Cherokee ", 
+" Jeep Grand Cherokee ", " Jeep Willys "), trans = c("Automática", 
+"Automática", "Automática", "Automática", "Automática", "Automática", 
+"Manual"), combu = c("Bencina", "Bencina", "Bencina", "Bencina", 
+"Bencina", "Bencina", "Bencina"), modelo2 = c("Jeep Grand Cherokee", 
+"Jeep Cherokee", "Jeep Grand Cherokee", "Jeep Grand Cherokee", 
+"Jeep Grand Cherokee", "Jeep Grand Cherokee", "Jeep Willys"), 
+    ano = c(2010L, 2002L, 1998L, 2002L, 2004L, 2004L, 1967L)), row.names = c("586", 
+"605", "609", "616", "745", "764", "767"), class = "data.frame")
